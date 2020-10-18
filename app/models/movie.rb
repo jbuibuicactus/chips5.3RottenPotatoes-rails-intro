f47b9522d0d0e848ce30e205c329031a71712e29
Justin Phi-Nhat Bui <legioncactus@berkeley.edu>
2020-10-18 02:40:46 +0000

    syntax error fixing with end keyword

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,8 +3,12 @@ class Movie < ActiveRecord::Base
   def Movie.all_ratings
     return ['G', 'PG', 'PG-13', 'R']
     
+  end
+    
     
   def Movie.with_ratings(ratings_selected)
     return Movie.where(ratings: ratings_selected)
+    
+  end
     
 end
